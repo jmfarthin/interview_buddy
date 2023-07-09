b2b3e497e1ea3e47b7302f6f9efa265fbfbb3bfe
Kevin Silveira <k.ironhide9@yahoo.com>
2023-07-09 06:27:37 -0400

    updated Chat.js

--- server/models/Chat.js
+++ server/models/Chat.js
@@ -1,4 +1,9 @@
 const { Schema, model, default: mongoose } = require('mongoose');
+const OpenAI = require('openai-api');
+const config = require('./config');
+
+// Kevins API key
+const openai = new OpenAI('config.openaiApiKey');
 
 
 const messageSchema = new Schema({
@@ -27,4 +32,25 @@ const chatSchema = new Schema({
 
 const Chat = mongoose.model('Chat', chatSchema);
 
-module.exports = Chat;
+async function generateResponse(input) {
+    try {
+        const response = await openai.complete({
+            engine: 'text-davinci-003',     // ChatGPT model 
+            prompt: input,
+            maxTokens: 100,     // maximum length of the generated response
+            n: 1, // number of responses to generate
+            stop: '\n', // Specify the stop sequence to end the response
+        });
+
+        return response.choices[0].text.trim();
+    } catch (error) {
+        // Handle any errors that occur during the API call
+        console.error('Error generating response:', error);
+        throw error;
+    }
+}
+
+module.exports = {
+    Chat,
+    generateResponse
+};
